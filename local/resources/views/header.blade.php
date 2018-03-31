1e555f0286650d31c152b2714a9f3bb997172cde
Cassy <cokwedadi@gmail.com>
2018-03-31 19:58:33 +0100

    Header

--- local/resources/views/header.blade.php
+++ local/resources/views/header.blade.php
@@ -32,23 +32,39 @@ $setid=1;
           <ul class="nav navbar-nav navbar-right <?php if($currentPaths=="index" or $currentPaths=="/"){?>sangvish_homepage<?php } else {?>sangvish_otherpage<?php } ?>">
 		  <!--<li class="active"><a href="#">Join as a pro</a></li>-->
             <?php if (Auth::guest()) {?>
-			
-            <li><a href="<?php echo $url;?>/register">Sign Up</a></li>
+			<!-- Added by Ninja 20180331 start here-->				
+						<li><a href="<?php echo $url;?>/">Home</a></li>									
+						<li><a href="<?php echo $url;?>/search">Security Personnel</a></li>						
+						<li><a href="<?php echo $url;?>/#">Find Jobs</a></li>						
+						<li><a href="<?php echo $url;?>/how-it-works">How It Works</a></li>																		
+			<!-- Added by Ninja 20180331 end here-->				
             <li><a href="<?php echo $url;?>/login">Login</a></li>
+            <li><a href="<?php echo $url;?>/register">Sign Up</a></li>						
             <?php } else { ?>
-			<li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"> {{ Auth::user()->name }}<b class="caret"></b></a>
+
+			<!-- Added by Ninja 20180331 start here-->	
+						<li><a href="<?php echo $url;?>/">Home</a></li>				
+						<li><a href="<?php echo $url;?>/shop">My Account</a></li>						
+						<li><a href="<?php echo $url;?>/search">Security Personnel</a></li>						
+						<li><a href="<?php echo $url;?>/#">Find Jobs</a></li>						
+						<li><a href="<?php echo $url;?>/how-it-works">How It Works</a></li>			
+						<li><a style="border-left: 1px solid #b3b0b0;" href="<?php echo $url;?>/#"><i class="fa fa-heart-o"></i></a></li>															
+						<li><a href="<?php echo $url;?>/#"><i class="fa fa-envelope-o"></i></a></li>															
+						<li><a href="<?php echo $url;?>/#"><i class="fa fa-bell-o"></i></a></li>																											
+			<!-- Added by Ninja 20180331 end here-->				
+			 <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">  Hi, {{ Auth::user()->name }}<b class="caret"></b></a>
               <ul class="dropdown-menu">
-                 <?php if(Auth::check()) { ?>
+               <?php if(Auth::check()) { ?>
                 <?php if(Auth::user()->admin==1) {?>
-				<li><a href="{{ url('admin/') }}" target="_blank">Admin Dashboard</a></li>
-				<?php } ?>
+
+								<li><a href="{{ url('admin/') }}" target="_blank">Admin Dashboard</a></li>
+						<?php } ?>
 								
-				<?php if(Auth::user()->admin==0) {?>
-				<li><a href="<?php echo $url;?>/dashboard">My Profile</a></li>
-				<li><a href="<?php echo $url;?>/my_bookings">My Bookings</a></li>
-				
-				<?php } ?>			
+						<?php if(Auth::user()->admin==0) {?>
+						<li><a href="<?php echo $url;?>/dashboard">My Dashboard</a></li>
+						<li><a href="<?php echo $url;?>/my_bookings">My Bookings</a></li>
+						<?php } ?>			
 								
 								
 			    <?php if(Auth::user()->admin==2) {
@@ -58,12 +74,14 @@ $setid=1;
 		 ->where('seller_email', '=',$sellmail)
 		 ->count();
 					?>
-				<li><a href="<?php echo $url;?>/dashboard">My Profile</a></li>
+				<li><a href="<?php echo $url;?>/dashboard">My Dashboard</a></li>
 				<li><a href="<?php echo $url;?>/my_bookings">My Bookings</a></li>
+				<!--
 				<li><a href="<?php echo $url;?>/myorder">My Order</a></li>
 				<li><a href="<?php if(empty($shcount)){?><?php echo $url;?>/addshop<?php } else { ?><?php echo $url;?>/shop<?php } ?>">My Shop</a></li>
 				<li <?php if(empty($shcount)){?>class="disabled"<?php } ?>><a href="<?php echo $url;?>/services" <?php if(empty($shcount)){?>class="disabled"<?php } ?>>My Services</a></li>
 				<li <?php if(empty($shcount)){?>class="disabled"<?php } ?>><a href="<?php echo $url;?>/gallery" <?php if(empty($shcount)){?>class="disabled"<?php } ?>>Shop Gallery</a></li>
+				-->
 				<li <?php if(empty($shcount)){?>class="disabled"<?php } ?>><a href="<?php echo $url;?>/wallet" <?php if(empty($shcount)){?>class="disabled"<?php } ?>>Wallet</a></li>
 				
 				<?php } ?>			
